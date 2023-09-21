c4ac4428f91a4d139d92aa8912f2d223e5d7ad10
Himanshu Upadhyay <7124054+ihimanshu19@users.noreply.github.com>
2023-09-21 10:35:11 +0530

    Fixing Linting issues[ihimanshu19]

--- com/test/githubactions/Test.java
+++ com/test/githubactions/Test.java
@@ -3,6 +3,12 @@ package com.test.githubactions;
  * This is test class to test github linting Actions.
  */
 public class Test {
+    private Test(){        
+    }
+
+    /**
+     * Main Function
+     */
     public static void main(final String[] args) {
         System.out.println("Hello World");
     }
